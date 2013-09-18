7b48452eb51b4d4ac1eecf7ce4456654e2a64984
Jonathan Lundholm <lootic@hotmail.com>
2013-09-18 12:32:24 +0200

    Fixed iterator so that it now iterates correctly, can't paint though

--- lab4/src/lab/S_ShapeIterator.java
+++ lab4/src/lab/S_ShapeIterator.java
@@ -10,28 +10,37 @@ public class S_ShapeIterator extends AbstractIterator {
 	private AbstractShape current;
 
 	S_ShapeIterator(AbstractSquare root) {
+		System.out.println("S_ShapeIterator Constructor ("+ root.toString()+")");
 		this.list = root.getListOfShapes(new ArrayList<AbstractShape>());
+		System.out.println(list.toString());
 		first();
 	}
 
 	@Override
 	public Object currentItem() {
+		if (current == null)
+			System.out.println("currentItem(), return: " + null);
+		else
+			System.out.println("currentItem(), return: " + current.toString());
 		return current;
 	}
 
 	@Override
 	public void first() {
+		System.out.println("first()");
 		this.iterator = list.iterator();
 		next();
 	}
 
 	@Override
 	public boolean isDone() {
+		System.out.println("isDone(), return: " + current == null);
 		return current == null;
 	}
 
 	@Override
 	public void next() {
+		System.out.println("next()");
 		if (iterator.hasNext()) {
 			current = iterator.next();
 		} else {
