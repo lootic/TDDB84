06e51652d591c1147599a33a4c1150cf91309e60
IanAdok <ian.adok@gmail.com>
2013-09-19 16:04:00 +0200

    fixed wierd stuff

--- lab2/src/lab/S_Prototype.java
+++ lab2/src/lab/S_Prototype.java
@@ -1,18 +1,10 @@
 package lab;
 
-<<<<<<< HEAD
 
-import java.awt.event.KeyEvent;
-
-public class S_Prototype extends 
-{
-	
+import java.awt.event.KeyEvent;	
 
 
-public class S_Prototype implements S_Creating {
-=======
 public class S_Prototype implements S_Creator {
->>>>>>> fa0e003f130c8ca9a08d696639f2a312632272dd
 	private Room room = new Room(0, 0);
 	private Door door = new Door(room, room);
 	private BoxDoor boxDoor = new BoxDoor(room, room);
@@ -23,10 +15,7 @@ public class S_Prototype implements S_Creator {
 		boxDoor = (BoxDoor) boxDoor.clone();
 		boxDoor.move(roomA, roomB);
 		return boxDoor;
-<<<<<<< HEAD
 
-=======
->>>>>>> fa0e003f130c8ca9a08d696639f2a312632272dd
 	}
 	@Override
 	public Door createDoor(Room roomA, Room roomB) {
